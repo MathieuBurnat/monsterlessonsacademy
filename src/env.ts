36884f555741095ca899a9619f2fce3d6ebf834d
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2024-06-16 15:20:43 +0200

    Create env.ts

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+// https://console.firebase.google.com
+export const firebaseConfig = {
+    apiKey: "api key",
+    authDomain: "auth domain",
+    projectId: "project id",
+    storageBucket: "storage bucket",
+    messagingSenderId: "sender id",
+    appId: "app id"
+};
